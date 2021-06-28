73824beedcf09ec8027c16771c54110b4de02781
dhowai <dhowai.dev@gmail.com>
2021-06-28 13:51:14 +0000

    Add navigation to homepage and make it responsive with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,14 @@ function openLogin() {
 function closeLogin() {
     document.getElementById("login").style.display = "none";
 }
+
+/* Navigation */
+
+function navFunction() {
+    let x = document.getElementById("mainNav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav"
+    }
+}
