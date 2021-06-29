1a2a62f1115cecb433e328c8426e7c52cabb7cda
dhowai <dhowai.dev@gmail.com>
2021-06-29 20:05:42 +0000

    Add contact form popup to footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,27 +22,39 @@ function navFunction() {
 /* Footer */
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("aboutModal");
+var modal2 = document.getElementById("contactModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("aboutBtn");
+var btn2 = document.getElementById("contactBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal1.style.display = "none";
+  modal2.style.display = "none";
 }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
