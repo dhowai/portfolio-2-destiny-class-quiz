466a82fc0c8b930eb8086bec34f6a1f3b4b41bf6
dhowai <dhowai.dev@gmail.com>
2021-07-12 16:00:45 +0000

    Add organization to scripts and seperate relevant functions to particular scripts

--- assets/js/mainscript.js
+++ assets/js/mainscript.js
@@ -1,24 +1,3 @@
-/* Landing Page */
-document.getElementById("loginBtn").addEventListener("click", loadHome)
-  
-document.getElementById("hero-image").style.display = "none";
-document.getElementById("mainNav").style.display = "none";
-document.getElementById("introd").style.display = "none";
-document.getElementById("charater-class").style.display = "none";
-document.getElementById("footer").style.display = "none";
-
-function loadHome() {
-  
-  document.getElementById("landingpage").style.display = "none";
-  document.getElementById("hero-image").style.display = "block";
-  document.getElementById("mainNav").style.display = "block";
-  document.getElementById("introd").style.display = "block";
-  document.getElementById("charater-class").style.display = "block";
-  document.getElementById("footer").style.display = "flex";
-}
-
-
-
 /* Navigation */
 
 function navFunction() {
@@ -29,55 +8,6 @@ function navFunction() {
         x.className = "nav"
     }
 }
-
-/* Home Page */
-
-// create needed constants
-const rememberMe = document.querySelector(".remember");
-const forgetMe = document.querySelector(".forget");
-const form = document.querySelector("form");
-const getName = document.querySelector("#entername");
-const submitBtn = document.querySelector("#submitname");
-const forgetBtn = document.querySelector("#forgetname");
-
-const h1 = document.querySelector(".hero-text");
-const h2 = document.querySelector("h2");
-forgetMe.style.display = "none";
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-});
-
-submitBtn.addEventListener("click", function () {
-  localStorage.setItem("name", getName.value);
-
-  nameDisplayCheck();
-});
-
-forgetBtn.addEventListener("click", function () {
-  localStorage.removeItem("name");
-
-  nameDisplayCheck();
-});
-
-function nameDisplayCheck() {
-  if (localStorage.getItem("name")) {
-    let name = localStorage.getItem("name");
-    h1.textContent = `Which Guardian Will You be, ${name}?`;
-    h2.textContent = `Welcome to the Destiny 2 Quiz, ${name}!`;
-
-    forgetMe.style.display = "block";
-    rememberMe.style.display = "none";
-  } else {
-    h1.textContent = `Which Guardian Will You be?`;
-    h2.textContent = `Welcome to the Destiny 2 Quiz!`;
-
-    forgetMe.style.display = "none";
-    rememberMe.style.display = "block";
-  }
-}
-
-document.body.onload = nameDisplayCheck;
 
 /* Footer */
 
